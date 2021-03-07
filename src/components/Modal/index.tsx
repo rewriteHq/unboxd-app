db41ca2c21a6dcef54a89c924b742d4e712a5efe
Yawmiej <yomlateef@yahoo.com>
2021-03-07 07:47:58 +0100

    Change Modal type definition to accommodate Compound component pattern

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,7 +8,7 @@ import {
 } from './styles';
 import { ModalComponentProps, ModalHeaderComponentProps } from './types';
 
-const Modal: React.FC<ModalComponentProps> | null = ({ show, children }) => {
+const Modal = ({ show, children }: ModalComponentProps) => {
   return show ? <ModalWrapper>{children}</ModalWrapper> : null;
 };
 
