2316b81dc53b850b36614751b688c1ad71bbddb9
Tolu Adesina <tolu.adesina@benestudio.co>
2021-05-12 05:46:20 +0100

    update homepage with needed content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,90 +3,45 @@ import { PageHeading } from '../../commons/Heading';
 import { HomeComponentProps } from './types';
 
 import Layout from '../../Layout';
-import { Paragraph, UnboxdCarousel, GetStarted } from './styles';
+import { UnboxdCarousel, GetStarted, UnorderedList } from './styles';
 import { BTNLink } from '../../commons/Button/styles';
 
 const Home: React.FC<HomeComponentProps> = () => {
 
-  const tabs = [
-    { name: "new home", active: false, icon: "🏛" },
-    { name: "social cause", active: false },
-    { name: "birthday", active: false },
-    { name: "graduation", active: true, icon: "🎉" },
-    { name: "anniversary", active: false },
-    { name: "wedding", active: false },
-    { name: "new business", active: false }
+  const steps = [
+    "Create a list with pictures of items you want",
+    "Share with loved ones",
+    "Receive part or full contributions"
   ]
 
-  const sample = [
-    {
-      name: "Work Space",
-      price: "245,999",
-      raised: "84,599.00",
-      image: "workstation"
-    },
-    {
-      name: "Macbook Pro",
-      price: "645,999",
-      raised: "324,099.00",
-      image: "laptop"
-    },
-    {
-      name: "Washing Machine",
-      price: "950,000",
-      raised: "284,898.00",
-      image: "washingmachine"
-    },
-    {
-      name: "HalfStation",
-      price: "350,000",
-      raised: "64,898.00",
-      image: "halfstation"
-    },
-    {
-      name: "HalfStation",
-      price: "350,000",
-      raised: "64,898.00",
-      image: "halfstation"
-    },
-    {
-      name: "HalfStation",
-      price: "350,000",
-      raised: "64,898.00",
-      image: "halfstation"
-    }
-  ]
+  const sample = ["ps4", "ipad", "camera", "sneakers", "throwpillows", "workspacechairs"]
 
   return (
     <Layout>
       <div className="container">
         <PageHeading centered large>
-          Whatever you want.
+          Gather funds for the things you need
         </PageHeading>
 
-        <Paragraph className="text-center">
-          List it, share it, and receive cash contributions from your loved ones.
-        </Paragraph>
+        <UnorderedList>
+          {steps.map((step, index) => <li key={index + 1}><span>{index + 1}</span>{step}</li>)}
+        </UnorderedList>
 
         <UnboxdCarousel>
-          <div className="carousel-tab">
-            <ul>
-              {tabs.map((item, index) => <li key={index} className={item.active ? "active list-item" : "list-item"}>{item.name} {item.active ? item.icon : null}</li>)}
-            </ul>
-          </div>
           <div className="carousel">
-            {sample.map((item, index) =>
+            {sample.slice(0, 3).map((item, index) =>
               <div key={index} className="card">
                 <div className="img-holder">
-                  <img src={`/assets/card-img/${item.image}.png`} alt={item.name} />
+                  <img src={`/assets/card-img/${item}.png`} alt={item} />
                 </div>
-                <div className="card-content">
-                  <h3 className="name">{item.name}</h3>
-                  <p className="price">₦{item.price}</p>
-                  <div className="progress">
-                    <p className="amount-raised">₦{item.raised} raised</p>
-                    <div className="progress-circle"></div>
-                  </div>
+              </div>
+            )}
+          </div>
+          <div className="carousel start">
+            {sample.slice(3).map((item, index) =>
+              <div key={index} className="card">
+                <div className="img-holder">
+                  <img src={`/assets/card-img/${item}.png`} alt={item} />
                 </div>
               </div>
             )}
