6cddcb82999ba9509451c298715cb88038df5b9e
Tolu Adesina <tolu.adesina@benestudio.co>
2021-05-14 22:17:09 +0100

    create component Loader

---
+++ src/commons/PageLoader/index.tsx
@@ -0,0 +1,27 @@
+import { Loader } from './style';
+
+const PageLoader = () => {
+  return (
+    <Loader>
+      <div className="windows8">
+        <div className="wBall" id="wBall_1">
+          <div className="wInnerBall"></div>
+        </div>
+        <div className="wBall" id="wBall_2">
+          <div className="wInnerBall"></div>
+        </div>
+        <div className="wBall" id="wBall_3">
+          <div className="wInnerBall"></div>
+        </div>
+        <div className="wBall" id="wBall_4">
+          <div className="wInnerBall"></div>
+        </div>
+        <div className="wBall" id="wBall_5">
+          <div className="wInnerBall"></div>
+        </div>
+      </div>
+    </Loader>
+  );
+}
+
+export default PageLoader;
