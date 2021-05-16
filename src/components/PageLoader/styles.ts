d8b7ab1594df8ee8b7eef11fa57e0b89ea0e9462
Tolu Adesina <tolu.adesina@benestudio.co>
2021-05-16 22:16:09 +0100

    create PageLoader and styling for the Loader wrapper

---
+++ src/components/PageLoader/styles.ts
@@ -0,0 +1,14 @@
+import Styled from 'styled-components';
+import Colors from '../../constants/Colors';
+
+export const LoaderWrapper = Styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 3;
+  width: 100%;
+  height: 100vh;
+  background: ${Colors.navy};
+  overflow: hidden;
+`;
