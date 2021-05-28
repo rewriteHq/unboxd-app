4fdb630e29b1a546293f557c945161ff37595b64
Tolu Adesina <tolu.adesina@benestudio.co>
2021-05-28 01:00:38 +0100

    fix: update the content on welcome modal

--- src/modules/contributor/pages/Event/components/WelcomeModal.tsx
+++ src/modules/contributor/pages/Event/components/WelcomeModal.tsx
@@ -15,17 +15,18 @@ type ComponentProps = {
 };
 
 const WelcomeModal = ({ show, close, list }: ComponentProps) => {
+  console.log(list);
   return (
     <Modal show={show} onClose={close}>
       <Modal.Centered>
         <WelcomeWrapper>
           <WelcomeHead>
             <Avatar src={list.coverImage} alt="User" />
-            <span className="name">Sandra</span>
+            <span className="name">{list.userID.firstname}</span>
           </WelcomeHead>
           <WelcomeBody>
             <p className="hello">Hello,</p>
-            <p className="note">{list.description ? list.description : "If you are here, it's because you count as one of my loved ones. On the list you're about to see are the items I deeply wish for my birthday"}</p>
+            <p className="note">{list.description ? list.description : "If you are here, it's because you count as one of my loved ones. On the list you're about to see, are the items I deeply wish for."}</p>
             <Button onClick={close}>Open List</Button>
           </WelcomeBody>
         </WelcomeWrapper>
