1879f1ff6fe4dc9acc88fc71a1610611bbbacac1
Tolu Adesina <tadesina90@gmail.com>
2021-08-23 10:00:07 +0100

    fix: month values for date validation

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -12,18 +12,18 @@ export const getDaysInMonth = (month: number, year: number) => {
 };
 
 export const months = [
-  { text: 'January', value: 0 },
-  { text: 'February', value: 1 },
-  { text: 'March', value: 2 },
-  { text: 'April', value: 3 },
-  { text: 'May', value: 4 },
-  { text: 'June', value: 5 },
-  { text: 'July', value: 6 },
-  { text: 'August', value: 7 },
-  { text: 'September', value: 8 },
-  { text: 'October', value: 9 },
-  { text: 'November', value: 10 },
-  { text: 'December', value: 11 },
+  { text: 'January', value: 1 },
+  { text: 'February', value: 2 },
+  { text: 'March', value: 3 },
+  { text: 'April', value: 4 },
+  { text: 'May', value: 5 },
+  { text: 'June', value: 6 },
+  { text: 'July', value: 7 },
+  { text: 'August', value: 8 },
+  { text: 'September', value: 9 },
+  { text: 'October', value: 10 },
+  { text: 'November', value: 11 },
+  { text: 'December', value: 12 },
 ];
 
 export const generateYears = (startDate: Date, range: number) => {
